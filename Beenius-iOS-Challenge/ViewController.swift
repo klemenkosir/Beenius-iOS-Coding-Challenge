f29b232842f8b1cbfee46ed7a0d5d92778186136
Klemen Kosir <klemen.kosir@icloud.com>
2019-05-31 13:29:42 +1000

    Initial Commit

---
+++ Beenius-iOS-Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Beenius-iOS-Challenge
+//
+//  Created by Klemen Košir on 31/05/2019.
+//  Copyright © 2019 Klemen Košir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
